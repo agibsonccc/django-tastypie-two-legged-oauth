191e2173e8d8f5c11c70e23f915635f9d4455483
Adam Gibson <agibson@clevercloudcomputing.com>
2012-05-24 08:00:41 -0400

    Got rid of duplicate declarations

--- setup.py
+++ setup.py
@@ -2,30 +2,9 @@ import os
  
 from distutils.core import setup
 from setuptools import setup, find_packages
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == "":
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
+
  
-package_dir = "osqa-context"
- 
-packages = find_packages('src')
-for dirpath, dirnames, filenames in os.walk(package_dir):
-    # ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith("."):
-            del dirnames[i]
-    if "__init__.py" in filenames:
-        packages.append(".".join(fullsplit(dirpath)))
+
  
 setup(
     name='django-tastypie-two-legged-oauth',
